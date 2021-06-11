ffb8723467f47ab3542fb325ddc0ec35d1aecf55
Vinay Patel <vinaypatel1610@gmail.com>
2021-06-11 16:54:26 +0530

    minor changes

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
-      <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h1>AarVi</h1>
+      <h3>Our Memories</h3>
+      {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
     </div>
   )
 }
