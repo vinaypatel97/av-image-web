841066fd02581b5030d7d382857e2cabd37b2163
Vinay Patel <vinaypatel1610@gmail.com>
2021-06-11 16:44:30 +0530

    Updates

---
+++ precache-manifest.3581d97ff11565b642d86956ecb6f478.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06a5114db5d2ad745b80f6c77dcf1b7c",
+    "url": "/av-image-web/index.html"
+  },
+  {
+    "revision": "e422aea88f8167627ed4",
+    "url": "/av-image-web/static/css/main.e3bd3a6e.chunk.css"
+  },
+  {
+    "revision": "0f0dad29076d78cc2e53",
+    "url": "/av-image-web/static/js/2.0c739371.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/av-image-web/static/js/2.0c739371.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e422aea88f8167627ed4",
+    "url": "/av-image-web/static/js/main.8d085868.chunk.js"
+  },
+  {
+    "revision": "3496726a3e92d3f3ecc9",
+    "url": "/av-image-web/static/js/runtime-main.439b2bc2.js"
+  }
+]);
